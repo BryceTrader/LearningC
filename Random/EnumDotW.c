f898cef5e833518f2a8d65354bcbc99c4996b191
BryceTrader <73015472+BryceTrader@users.noreply.github.com>
2021-10-13 16:26:14 +0900

    Create EnumDotW.c

---
+++ Random/EnumDotW.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+char* findDay(int x) {
+    switch(x) {
+        case 0:
+            return "Monday";
+        case 1:
+            return "Tuesday";
+        case 2:
+            return "Wednesday";
+        case 3:
+            return "Thursday";
+        case 4:
+            return "Friday";
+        case 5:
+            return "Saturday";
+        case 6:
+            return "Sunday";
+        default:
+            return "Error";
+    }
+}
+
+int main() {
+    typedef enum {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday} WeekDay_E;
+    WeekDay_E today = 0;
+    WeekDay_E tomorrow = 1;
+    
+    for(int i = 0; i < 14; i++) {
+        char day[10]; 
+        strcpy(day, findDay(today));
+        printf("Today is %s\n", day);
+        
+        if(tomorrow < Sunday && today < Sunday) {
+            today = tomorrow;
+            tomorrow = today + 1; 
+        } else {
+            tomorrow = Monday;
+        }
+    }
+    return 0;
+}
